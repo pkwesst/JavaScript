09158e645907e127fc6584e06768506793657def
Seohyun <90893579+pkwesst@users.noreply.github.com>
2022-04-10 23:33:56 +0900

    Update and rename Ex_write.html to Ex_write.js

---
+++ Ex_write.js
@@ -0,0 +1,15 @@
+// 1. 웹 페이지에 직접 출력
+document.write("안녕하세요");
+
+// 1-2. 태그도 적용 가능
+document.write("<h1>안녕하세요</h1>");
+
+// 2. console창에 출력하기
+console.log("안녕하세요, 여기는 consol창입니다.");
+
+// 2-2. Log외의 다른 기능
+console.warn("경고");
+console.error("에러");
+
+// 3. 알림창에 출력
+alert("안녕하세요, 여기는 알림창입니다.");
