49f73ecb2bd8bdf7aed3293ec61006fd77035195
Seohyun <90893579+pkwesst@users.noreply.github.com>
2022-04-10 23:36:42 +0900

    Update Ex_while_random.js

--- Ex_while_random.js
+++ Ex_while_random.js
@@ -5,14 +5,14 @@
 
 // 2. while 반복문
     while(true){
-        const input_num = parseInt(prompt("숫자를 입력해주세요!"));    // 사용자에게 입력 받은 stirng을 number data type으로 변환(parseInt)
+        const input_num = parseInt(prompt("숫자를 입력 해주세요!"));    // 사용자에게 입력 받은 stirng을 number data type으로 변환(parseInt)
 
         if (input_num == num_random){   
             alert("정답입니다!! 축하합니다!");
             break;
         }else if( ran_num > input_num){
-            alert("입력한 숫자보다 큰 수 입니다.");
+            alert("입력한 숫자보다 큰 수입니다.");
         }else {
-            alert("입력한 숫자보다 작은 수 입니다.");
+            alert("입력한 숫자보다 작은 수입니다.");
         }
     }
